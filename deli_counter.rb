413cdc2127040a8492011062f0e32faa458f861b
Bperez4197 <bryceperez31@gmail.com>
2017-11-30 09:13:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,16 @@
 # Write your code here.
-katz_deli = []
 
 def line(katz_deli)
+  if katz_deli == 0
+    puts "The line is currently empty."
+  else
+    message = "The line is currently:"
+
+    katz_deli.each_with_index do |value, index|
+      message += "#{index.to_i+1}. #{value}"
+    end
+    puts "#{message}"
+  end
 end
 
 def take_a_number(katz_deli, name)
@@ -9,4 +18,3 @@ end
 
 def now_serving(katz_deli)
 end
-
